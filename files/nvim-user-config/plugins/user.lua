8f664995bd7195957f13c9b05888e95c0b149a8b
Xander Soldaat <xander@botbench.com>
2023-06-12 20:26:40 +0200

    Added files folder

---
+++ files/nvim-user-config/plugins/user.lua
@@ -0,0 +1,20 @@
+
+return {
+  { 
+    "catppuccin/nvim", 
+     name = "catppuccin", 
+    lazy = false, 
+    flavour = "mocha"
+  },
+
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+}
